0f43a6d929667e8a2a347720804de1c5f8544d76
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-17 10:28:30 +0800

    debug

--- router/router.js
+++ router/router.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const controllers = require("../controller/controller");
 const router = express.Router();
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
 
-router.get("/allusers", controllers.getinfo)
-router.get("/seed", controllers.seed)
-router.put("/create", controllers.createInfo)
-router.post("/search", controllers.searchInfo)
-router.patch("/update", controllers.updateInfo)
-router.delete("/delete", controllers.deleteInfo)
-router.post("/searchbydate", controllers.searchByDate)
+router.get("/allusers", controllers.getinfo);
+router.get("/seed", controllers.seed);
+router.put("/create", controllers.createInfo);
+router.post("/search", controllers.searchInfo);
+router.patch("/update", controllers.updateInfo);
+router.delete("/delete", controllers.deleteInfo);
+router.post("/searchbydate", controllers.searchByDate);
 // router.get("/deploy", controllers.deploy)
-
 
 module.exports = router;
