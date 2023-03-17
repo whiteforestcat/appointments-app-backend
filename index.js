fb61711073ec102d39b44d2d711fe9bd84efa3a9
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-17 10:36:14 +0800

    revert to env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-connectDB(`mongodb://127.0.0.1:27017/somedb`);
+connectDB(process.env.MONGODB_URI);
 
 app.use("/api", user);
 
