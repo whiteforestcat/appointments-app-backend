f80c1aa76e16f8aa1c493fd1cdb94ecd5ade1c8b
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-17 10:22:42 +0800

    Revert "deploy message"
    This reverts commit 318796dbf754d4832d9c30989e0464ff9e3b0c68.

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-require("dotenv").config();
-
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./db/db");
-const user = require("./router/router");
+
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-connectDB(process.env.MONGODB_URI);
+const PORT = process.env.PORT || 5000;
 
-app.use("/api", user);
 
-app.listen(5000);
+app.listen(PORT, () => console.log("Server is running in port", PORT));
+
